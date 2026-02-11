69ffa8dd0844210ed279b63ec074b206bb0fe16a
Auto Agent <auto-agent@example.com>
2026-02-11 22:24:32 +0530

    Update base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
+    base: '/tfc-pay-android-app/',
     build: {
         outDir: 'dist',
         sourcemap: false,
